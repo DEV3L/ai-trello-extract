7ca672044560628e234dadf2a4f2485eeacd5ca4
DEV3L <jus.beall@gmail.com>
2024-08-13 09:03:33 -0400

    Add main file contents for trello extract

--- run_end_to_end.py
+++ run_end_to_end.py
@@ -1,42 +1,23 @@
 from loguru import logger
 
-from ai_trello_extract.assistants.assistant_service import (
-    AssistantService,
-)
-from ai_trello_extract.chats.chat import Chat
-from ai_trello_extract.clients.openai_api import OpenAIClient, build_openai_client
-from ai_trello_extract.exporters.files.files_exporter import FilesExporter
+from ai_trello_extract.clients.trello_client import get_trello_client
+from ai_trello_extract.orchestrators.orchestration_service import OrchestrationService
+from ai_trello_extract.services.trello_service import TrelloService
+from ai_trello_extract.settings import get_settings
 
 
 def main():
-    FilesExporter("about.txt").export()
+    settings = get_settings()
+    logger.info(settings.trello_api_key)
+    orchestration_service = OrchestrationService(TrelloService(get_trello_client(settings)))
 
-    assistant_name = "AI-Assistant-Manager-Test"
-    logger.info(f"Building {assistant_name}")
-
-    client = OpenAIClient(build_openai_client())
-    service = AssistantService(client, "You are a helpful assistant")
-
-    logger.info("Removing existing assistant and category files")
-    service.delete_assistant()
-
-    assistant_id = service.get_assistant_id()
-    logger.info(f"Assistant ID: {assistant_id}")
-
-    chat = Chat(client, assistant_id)
-    chat.start()
-
-    message = "What is the AI Assistant Manager?"
-    print(f"\nMessage:\n{message}")
-
-    start_response = chat.send_user_message(message)
-    print(f"\n{service.assistant_name}:\n{start_response}")
-
-    service.delete_assistant()
+    try:
+        markdown_file_name = orchestration_service.write_board_markdown_to_file(settings.trello_board_name, "bin")
+        logger.info(f"Markdown file written to {markdown_file_name}")
+        # logger.info(orchestration_service.write_board_json_to_file(settings.trello_board_name, "bin"))
+    except RuntimeError as e:
+        logger.error(e)
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        logger.info(f"Error: {e}")
+    main()
